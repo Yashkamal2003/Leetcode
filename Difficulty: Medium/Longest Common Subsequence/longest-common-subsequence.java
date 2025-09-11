6f869403f08fc8376bfb9c56fcfdba4ab19bc5fe
yash kamal <121361954+Yashkamal2003@users.noreply.github.com>
2025-09-11 12:33:31 -0600

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.java
+++ Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.java
@@ -4,22 +4,35 @@ class Solution {
         int n= s1.length();
         int m= s2.length();
         int [][] dp=new int[n+1][m+1];
-        for( int i=1;i<=n;i++){
+     /*   for( int i=1;i<=n;i++){
             for( int j=1;j<=m;j++) {
                 dp[i][j]=-1;
             }
-        }
+        }*/
         return  lcsutil(n,m,s1,s2,dp);
     }
     static int lcsutil( int n,int m,String s1, String s2,int[][]dp){
        
-        if( n==0||m==0)
+       /* if( n==0||m==0)
         return 0;
         if (dp[n][m]!=-1){
             return dp[n][m];
         }
         if(s1.charAt(n-1)==s2.charAt(m-1))
         return dp[n][m]=1+ lcsutil(n-1,m-1,s1,s2,dp);
-        else  return dp[n][m]=Math.max(lcsutil(n-1,m,s1,s2,dp),lcsutil(n,m-1,s1,s2,dp));
-    }
+        else  return dp[n][m]=Math.max(lcsutil(n-1,m,s1,s2,dp),lcsutil(n,m-1,s1,s2,dp));*/
+       for( int i=1;i<=n;i++){
+           for(int j=1;j<=m;j++){
+               if(s1.charAt(i-1)== s2.charAt(j-1)){
+                   dp[i][j]= dp[i-1][j-1]+1;
+                   
+               }else{
+                   dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+               }
+           }
+       }
+        return dp[n][m];
+       
+       
+          }
 }
